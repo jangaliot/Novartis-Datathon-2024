4c46f037c2efe3bc06611c569572cda6aca22879
nachoogriis <nachogrismartin@gmail.com>
2024-11-30 12:19:44 +0100

    Helper updates

--- src/helper.py
+++ src/helper.py
@@ -7,6 +7,8 @@ import pandas as pd
 from pathlib import Path
 from typing import Tuple
 import pickle
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
 
 
 def _CYME(df: pd.DataFrame) -> float:
@@ -55,11 +57,29 @@ def compute_metric(submission: pd.DataFrame) -> Tuple[float, float]:
 
 
 # Load model
-model_path = "../models/weights/model_imputeKnn_scale.pkl"
+model_path = "../models/weights/model_imputeKnn_imputeKnn_scale_xgboost_hpt.pkl"
 model = pickle.load(open(model_path, "rb"))
 
 # Load data
-train_data = pd.read_csv("../data/processed/train_data_processed.csv")
+train_data = pd.read_csv("../data/processed/train_data_processed_imputeKnn_scale.csv")
+train_data_raw = pd.read_csv("../data/raw/train_data.csv")
+
+# Inspección inicial
+print("Dimensiones del dataset:", train_data.shape)
+print(train_data.head())
+
+# Identificar la variable objetivo y las características
+target_col = "target"
+features = [col for col in train_data.columns if col != target_col]
+
+# Preprocesamiento: manejar valores categóricos y nulos
+# Convertir categorías a valores numéricos
+label_encoders = {}
+for col in train_data.select_dtypes(include=['object']).columns:
+    if col in features:
+        le = LabelEncoder()
+        train_data[col] = le.fit_transform(train_data[col].astype(str))
+        label_encoders[col] = le
 
 # Split into train and validation set
 validation = train_data.sample(frac=0.2, random_state=42)
@@ -72,15 +92,21 @@ validation["prediction"] = model.predict(validation[features])
 
 # Assign column ["zero_actuals"] in the depending if in your
 # split the cluster_nl has already had actuals on train or not
-train_clusters = train_data["cluster_nl"].unique()
+train_clusters = train_data_raw["cluster_nl"].unique()
+validation['cluster_nl'] = train_data_raw["cluster_nl"]
 validation["zero_actuals"] = ~validation["cluster_nl"].isin(train_clusters)
 
 # Optionally check performance
 print("Performance:", compute_metric(validation))
 
 # Prepare submission
-submission_data = pd.read_parquet(PATH / "submission_data_processed.csv")
-submission = pd.read_csv(PATH / "submission_template.csv")
+submission_data = pd.read_csv("../data/processed/submission_data_processed_imputeKnn_scale.csv")
+submission = pd.read_csv("../data/submissions/submission_template.csv")
+
+categorical_columns = submission_data.select_dtypes(include=['object']).columns
+for col in categorical_columns:
+    if col in label_encoders:  # Usa los label encoders existentes
+        submission_data[col] = label_encoders[col].fit_transform(submission_data[col].astype(str))
 
 # Fill in 'prediction' values of submission
 submission["prediction"] = model.predict(submission_data[features])
